9cb6f0e73a1892e71daf2ad1f0b5d80d11f360a0
binbinskywalker <binbin2010qsc@msn.cn>
2022-07-28 18:09:12 +0800

    remake autostart.sh

--- build/autostart.sh
+++ build/autostart.sh
@@ -3,5 +3,4 @@ gatewayhome=${HOME}/work/gateway-daemon/build
 export gatewayhome
 export LD_LIBRARY_PATH=${gatewayhome}/lib:$LD_LIBRARY_PATH
 cd ${gatewayhome}
-#nohup ./hkcamera &
-./gateway-daemon
+nohup ./gateway-daemon &
